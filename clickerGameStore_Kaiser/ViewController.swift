83aa332ca5563979700b3fb5137ba6a4ae797731
Andrew Kaiser <Andrewkaiser2006@gmail.com>
2023-10-03 09:50:10 -0500

    UI

--- clickerGameStore_Kaiser/ViewController.swift
+++ clickerGameStore_Kaiser/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var points = 0
+    var clickCount = 1
+    @IBOutlet weak var pointsOutlet: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func clickedAction(_ sender: Any) {
+        points+=clickCount
+        pointsOutlet.text = "Points: \(points)"
+    }
+    
+    
+    @IBAction func storeAction(_ sender: Any) {
+        print("Going to store")
+        performSegue(withIdentifier: "toStore", sender: self)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nvc = segue.destination as! storeViewController
+        nvc.delegate = self
+        nvc.storePointsOutlet.text = "Points: \(points)"
+    }
+    
+    
 
 }
 
---
+++ clickerGameStore_Kaiser/storeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  storeViewController.swift
+//  clickerGameStore_Kaiser
+//
+//  Created by ANDREW KAISER on 10/3/23.
+//
+
+import UIKit
+
+class storeViewController: UIViewController {
+
+    var delegate: ViewController!
+    @IBOutlet weak var storePointsOutlet: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
